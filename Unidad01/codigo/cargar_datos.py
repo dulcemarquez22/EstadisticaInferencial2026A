c871c8222ac430dadb363b2347f3c25abb83ef66
dulcemarquez22 <dulcemarquez804@gmail.com>
2026-08-31 13:57:20 -0600

    Import pandas and load CSV data
    Added code to load CSV data using pandas.

--- Unidad01/codigo/cargar_datos.py
+++ Unidad01/codigo/cargar_datos.py
@@ -1,1 +1,4 @@
-
+import pandas as pd
+link_csv = "https://raw.githubusercontent.com/dulcemarquez22/EstadisticaInferencial2026A/refs/heads/main/Unidad01/Problemario/datos_problema_01.csv"
+df = pd.read_csv(link_csv)
+df 
